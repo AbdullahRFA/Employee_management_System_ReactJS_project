552cfddd82ded0612fbf999fd3b588625e1a5090
AbdullahRFA <shakibrybmn@gmail.com>
2026-02-14 21:32:28 +0600

    design tasklistnumbers component

--- ems/src/components/Dashboard/EmployeeDashboard.jsx
+++ ems/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,9 +1,13 @@
-import Header from "../Others/Header"
+import Header from "../Others/Header";
+import TaskListNumbers from "../Others/TaskListNumbers";
+import TaskList from "../TaskList/TaskList"
 
 function EmployeDashboard(){
     return(
         <div className="p-10 h-screen bg-[#1C1C1C]">
                 <Header />
+                <TaskListNumbers />
+                <TaskList />
         </div>
     )
 }
---
+++ ems/src/components/TaskList/TaskList.jsx
@@ -0,0 +1,8 @@
+// function TaskList(){
+//     return(
+//         <div className="h-[55%] w-full bg-red-500 mt-10">
+
+//         </div>
+//     )
+// }
+// export default TaskList
